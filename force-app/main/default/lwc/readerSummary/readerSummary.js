a27b27761ca0f74b87f6e241da080e5eb72f340b
egorapaniuk <egor.apaniuk@vrpconsulting.com>
2024-02-19 15:56:04 +0100

    Added Close Active Talons Button

--- force-app/main/default/lwc/readerSummary/readerSummary.js
+++ force-app/main/default/lwc/readerSummary/readerSummary.js
@@ -1,18 +1,42 @@
 import { LightningElement, api, wire } from 'lwc';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 import getReaderSummary from '@salesforce/apex/ReaderBookSummaryController.getReaderSummary';
+import handleCloseActiveBRBs from '@salesforce/apex/ReaderBookSummaryController.handleCloseActiveBRBs';
+import { refreshApex } from '@salesforce/apex';
 
 export default class ReaderSummary extends LightningElement {
     @api recordId;
-    currentReadingCount;
-    totalReadCount;
+    activeRBRCount;
+    totalRBRCount;
 
     @wire(getReaderSummary, { readerId: '$recordId' })
     wiredReaderSummary({ error, data }) {
         if (data) {
-            this.currentReadingCount = data.currentlyReadingCount;
-            this.totalReadCount = data.totalReadCount;
+            this.activeRBRCount = data.activeRBRCount;
+            this.totalRBRCount = data.totalRBRCount;
         } else if (error) {
             console.error(error);
         }
+    }
+
+    handleCloseActiveBRBs() {
+        console.log(this.recordId);
+        handleCloseActiveBRBs({ readerId: this.recordId })
+            .then(() => {
+                this.showToast('Success', Constants.ACTIVE_BRB_UPDATED_TO_COMPLETE, 'success');
+                return refreshApex(this.wiredReaderSummary);
+            })
+            .catch(error => {
+                this.showToast('Error', 'Error closing active talons: ' + error.body.message, 'error');
+            });
+    }
+
+    showToast(title, message, variant) {
+        const event = new ShowToastEvent({
+            title: title,
+            message: message,
+            variant: variant,
+        });
+        this.dispatchEvent(event);
     }
 }
