dab7251bfcb983681453097f1249aa6eb0338475
egorapaniuk <egor.apaniuk@vrpconsulting.com>
2024-02-22 13:25:45 +0100

    Added comments, improved formatting

--- force-app/main/default/lwc/readerSummary/readerSummary.js
+++ force-app/main/default/lwc/readerSummary/readerSummary.js
@@ -1,36 +1,80 @@
-import { LightningElement, api, wire } from 'lwc';
+import { LightningElement, api, wire, track } from 'lwc';
 import { ShowToastEvent } from 'lightning/platformShowToastEvent';
+import { refreshApex } from '@salesforce/apex';
+import { subscribe, unsubscribe } from 'lightning/empApi';
+
 import getReaderSummary from '@salesforce/apex/ReaderBookSummaryController.getReaderSummary';
 import handleCloseActiveBRBs from '@salesforce/apex/ReaderBookSummaryController.handleCloseActiveBRBs';
-import { refreshApex } from '@salesforce/apex';
+//? QUESTION: каким должен быть порядок импортов в ЛВЦ?	| Вроде такой форматинг правильный.
 
+const ALL_TALONS_CLOSED_MESSAGE = 'All Talons were Closed!';
+
+/**
+ * description      This component 1) shows summary: number of All and Active Reader's RBRs
+ *                  2) "Completes" all Reader's RBRs with Status__c = 'Active' on handleCloseActiveBRBs
+ * @author          Egor Apaniuk
+ * @since           22/02/2024
+ */
 export default class ReaderSummary extends LightningElement {
     @api recordId;
-    activeRBRCount;
-    totalRBRCount;
 
+    data;
+    isDataLoaded = false;
+    isButtonDisabled = false;
+    wiredReaderSummaryResult;
+    channelName = '/event/ReadEvent__e';
+
+    subscription = {};
+    async connectedCallback () {
+        this.subscription = await subscribe(this.channelName, -1, response => {
+            return refreshApex(this.wiredReaderSummaryResult);
+        })
+    }
+
+    disconnectedCallback () {
+        unsubscribe(this.subscription)
+    }
+
+    /**
+     * description      This reactive method gets number of all and Active Reader's RBRs.
+     * @author          Egor Apaniuk
+     * @since           22/02/2024
+     */
     @wire(getReaderSummary, { readerId: '$recordId' })
-    wiredReaderSummary({ error, data }) {
+    getReaderSummary(result) {
+        this.wiredReaderSummaryResult = result;
+        const {data, error} = result;
         if (data) {
-            this.activeRBRCount = data.activeRBRCount;
-            this.totalRBRCount = data.totalRBRCount;
-        } else if (error) {
-            console.error(error);
+            this.isDataLoaded= true;
+            this.data = data;
+
         }
     }
 
+    /**
+     * description      This handler sets all Active Reader's RBRs Status__c to 'Complete'.
+     * @author          Egor Apaniuk
+     * @since           22/02/2024
+     */
     handleCloseActiveBRBs() {
-        console.log(this.recordId);
         handleCloseActiveBRBs({ readerId: this.recordId })
             .then(() => {
-                this.showToast('Success', Constants.ACTIVE_BRB_UPDATED_TO_COMPLETE, 'success');
-                return refreshApex(this.wiredReaderSummary);
+                this.showToast('Success', ALL_TALONS_CLOSED_MESSAGE, 'success');
+                return refreshApex(this.wiredReaderSummaryResult);
             })
             .catch(error => {
-                this.showToast('Error', 'Error closing active talons: ' + error.body.message, 'error');
+                this.showToast('Error', 'Error closing active talons: ' + error?.body?.message, 'error');
             });
     }
 
+    /**
+     * description  This method shows toast messages
+     * @param       title
+     * @param       message
+     * @param       variant
+     * @author      Egor Apaniuk
+     * @since       22/02/2024
+     */
     showToast(title, message, variant) {
         const event = new ShowToastEvent({
             title: title,
